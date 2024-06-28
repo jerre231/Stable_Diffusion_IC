dcb9ecb1c1a99d412f3dff05519c55c6be36a19f
jerre231 <joaoscofield.20231@poli.ufrj.br>
2024-06-27 22:51:44 -0300

    compresse_ratio_csv

--- Scripts/comp_lossless.py
+++ Scripts/comp_lossless.py
@@ -1,12 +1,12 @@
 from PIL import Image
 import os, glob
-from compression_data import compression_ratio 
+from compression_data import compression_ratio
 
 
 def imageCompressor(dataFrame,final_format,compressed_adress="Comprimida",compression_quality = 95):
 
     # Variáveis auxiliares e contadores
-    
+
     class1_total = 0
     class1_size = 0
     class2_total = 0
@@ -18,20 +18,20 @@ def imageCompressor(dataFrame,final_format,compressed_adress="Comprimida",compre
     pilImages = {}       # Lista de images no formato da biblioteca PIL
     image_class=[]       # Define a classe da imagem
     compressRatio=[]     # Lista de Taxas de Compressão
-    compressMeanUnple=[] # Auxiliar cálculo da média da taxa de compressão de imagens classe 1 
-    compressMeanUnple=[] # Auxiliar cálculo da média da taxa de compressão de imagens classe 2 
+    compressMeanUnple=[] # Auxiliar cálculo da média da taxa de compressão de imagens classe 1
+    compressMeanUnple=[] # Auxiliar cálculo da média da taxa de compressão de imagens classe 2
     final_data=[]        # Lista com (Imagem, Taxa de compressão)
-    
+
 
     # Extraindo imagens .PNG do diretório
 
     all_imgs = dataFrame[dataFrame.columns[0]].values.tolist() # Lista com todas as imagens
     number_of_imgs = len(all_imgs)                             # Número total de imagens
-    
+
     # Loop de compressão das imagens:
 
     for i in range(number_of_imgs):
-        
+
         pilImages[i]=Image.open(all_imgs[i])  # Abre a imagem com a biblioteca PIL
         img_dimension = pilImages[i].size     # Dimensão da imagem
 
@@ -47,22 +47,22 @@ def imageCompressor(dataFrame,final_format,compressed_adress="Comprimida",compre
         # Comprime imagens no formato PNG
 
         if(final_format.lower() == "png"):
-            
-            pilImages[i].save(compressed_img_adress,optimize=True) 
+
+            pilImages[i].save(compressed_img_adress,optimize=True)
 
         # Comprime imagens no formato JPG
-        
+
         elif(final_format.lower() == "jpg"):
-            
-            pilImages[i].save(compressed_img_adress,optimize=True,quality = compression_quality) 
-        
+
+            pilImages[i].save(compressed_img_adress,optimize=True,quality = compression_quality)
+
         else:
 
             print(f"Formato final '{final_format}' não suportado.")
 
 
         # Análise dos resultados da compressão
-        
+
         compressRatio.append(compression_ratio(all_imgs[i],compressed_img_adress))
 
         image_class = dataFrame[dataFrame.columns[1]].values.tolist()
@@ -80,7 +80,7 @@ def imageCompressor(dataFrame,final_format,compressed_adress="Comprimida",compre
 
         class1_mean = round((class1_total/class1_size),1)
         class2_mean = round((class2_total/class2_size),1)
-    
+
         print(f"A média da classe 1 foi : {class1_mean} %")
         print(f"A média da classe 2 foi : {class2_mean} %\n")
 
@@ -92,13 +92,3 @@ def imageCompressor(dataFrame,final_format,compressed_adress="Comprimida",compre
     print(final_data)
 
     return(compressRatio)
-
-
-
-
-
-
-    
-        
-
-
