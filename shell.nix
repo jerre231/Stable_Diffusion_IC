d337bcea429a659ed0617f1d7a90743a3ada7769
jerre231 <joaoscofield.20231@poli.ufrj.br>
2024-08-09 04:23:22 -0300

    images_update

--- shell.nix
+++ shell.nix
@@ -1,22 +1,34 @@
 { pkgs ? import <nixpkgs> {} }:
 
-pkgs.mkShell {
-  buildInputs = [
-    pkgs.python3
-    pkgs.python3Packages.jupyter
-    pkgs.python3Packages.jupyterlab
-    pkgs.python3Packages.faiss
-    pkgs.python3Packages.requests
-    pkgs.python3Packages.numpy
-    pkgs.python3Packages.pandas
-    pkgs.python3Packages.pillow
-    pkgs.python3Packages.matplotlib
-    pkgs.python3Packages.seaborn
-    pkgs.python3Packages.scipy
-    pkgs.vim
+let
+  unstable = import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/nixpkgs-unstable.tar.gz";
+  }) {};
+
+in pkgs.mkShell {
+     buildInputs = [
+     pkgs.python3
+     pkgs.python3Packages.jupyter
+     pkgs.python3Packages.jupyterlab
+     pkgs.python3Packages.faiss
+     pkgs.python3Packages.requests
+     pkgs.python3Packages.numpy
+     pkgs.python3Packages.pandas
+     pkgs.python3Packages.pillow
+     pkgs.python3Packages.matplotlib
+     pkgs.python3Packages.seaborn
+     pkgs.python3Packages.scipy
+     pkgs.python3Packages.ollama
+     pkgs.vim
+     unstable.ollama-rocm
   ];
 
   shellHook = ''
     export PYTHONPATH=$PYTHONPATH:${pkgs.python3.sitePackages}
+    echo "Starting Ollama..."
+    nohup ollama serve &>/dev/null &
+   
+    echo "Pulling llava:"
+    ollama pull llava
   '';
 }
